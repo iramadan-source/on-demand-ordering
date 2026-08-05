d07f5e4c64eb30643e80793d07126dba664bea6b
Idris Ramadan <YOUR_GITHUB_EMAIL@example.com>
2026-08-05 14:31:59 +0300

    Fix Orders page

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -6,13 +6,11 @@ import OrderForm from "@/components/orders/OrderForm";
 export default function OrdersPage() {
   return (
     <AppLayout>
-
       <h1 className="mb-10 text-5xl font-bold">
         New Order
       </h1>
 
       <OrderForm />
-
     </AppLayout>
   );
 }
